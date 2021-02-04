8c1e5d7a81d622287650cf075efcec777082d6e4
Double-oxygeN <mail@double-oxygen.net>
2021-02-04 15:32:41 +0900

    error handling

--- MikanLoaderPkg/main.nim
+++ MikanLoaderPkg/main.nim
@@ -11,6 +11,10 @@ import lib/[
   graphicsoutput
 ]
 
+proc halt =
+  while true:
+    asm "hlt"
+
 type
   MemoryMap = object
     bufferSize: culonglong
@@ -36,7 +40,7 @@ proc openRootDir(imageHandle: EfiHandle; root: var ptr EfiFileProtocol): EfiStat
     loadedImage: ptr EfiLoadedImageProtocol
     fs: ptr EfiSimpleFileSystemProtocol
 
-  discard gBS[].openProtocol(
+  result = gBS[].openProtocol(
     imageHandle,
     addr gEfiLoadedImageProtocolGuid,
     cast[ptr pointer](addr loadedImage),
@@ -44,7 +48,9 @@ proc openRootDir(imageHandle: EfiHandle; root: var ptr EfiFileProtocol): EfiStat
     EfiHandle(nil),
     efiOpenProtocolByHandleProtocol)
 
-  discard gBS[].openProtocol(
+  if efiError(result): return
+
+  result = gBS[].openProtocol(
     loadedImage.deviceHandle,
     addr gEfiSimpleFileSystemProtocolGuid,
     cast[ptr pointer](addr fs),
@@ -52,21 +58,25 @@ proc openRootDir(imageHandle: EfiHandle; root: var ptr EfiFileProtocol): EfiStat
     EfiHandle(nil),
     efiOpenProtocolByHandleProtocol)
 
-  fs[].openVolume(fs, addr root)
+  if efiError(result): return
+
+  result = fs[].openVolume(fs, addr root)
 
 proc openGop(imageHandle: EfiHandle; gop: var ptr EfiGraphicsOutputProtocol): EfiStatus =
   var
     numGopHandles: culonglong = 0'u64
     gopHandles: ptr EfiHandle = nil
 
-  discard gBS[].locateHandleBuffer(
+  result = gBS[].locateHandleBuffer(
     ByProtocol,
     addr gEfiGraphicsOutputProtocolGuid,
     nil,
     addr numGopHandles,
     addr gopHandles)
 
-  discard gBS[].openProtocol(
+  if efiError(result): return
+
+  result = gBS[].openProtocol(
     gopHandles[],
     addr gEfiGraphicsOutputProtocolGuid,
     cast[ptr pointer](addr gop),
@@ -74,24 +84,37 @@ proc openGop(imageHandle: EfiHandle; gop: var ptr EfiGraphicsOutputProtocol): Ef
     EfiHandle(nil),
     efiOpenProtocolByHandleProtocol)
 
+  if efiError(result): return
+
   freePool gopHandles
 
 
 proc uefiMain(imageHandle: EfiHandle; systemTable: ptr EfiSystemTable): EfiStatus {.efidecl.} =
+  var status: EfiStatus
+
   print fastwidestr("Hello, Mikan World!\n")
 
   var
     memmapBuf: array[4_096 * 4, uint8]
     memmap = MemoryMap(bufferSize: uint64(sizeof(memmapBuf)), buffer: addr memmapBuf[0])
 
-  discard getMemoryMap(memmap)
+  status = getMemoryMap(memmap)
+  if efiError(status):
+    print fastwidestr("failed to get memory map: %r\n"), status
+    halt()
 
   var rootDir: ptr EfiFileProtocol
-  discard openRootDir(imageHandle, rootDir)
+  status = openRootDir(imageHandle, rootDir)
+  if efiError(status):
+    print fastwidestr("failed to open root directory: %r\n"), status
+    halt()
 
   # GOP でピクセル描画
   var gop: ptr EfiGraphicsOutputProtocol
-  discard openGop(imageHandle, gop)
+  status = openGop(imageHandle, gop)
+  if efiError(status):
+    print fastwidestr("failed to open GOP: %r\n"), status
+    halt()
 
   var
     frameBuffer = cast[ptr UncheckedArray[uint8]](gop.mode.frameBufferBase)
@@ -101,13 +124,18 @@ proc uefiMain(imageHandle: EfiHandle; systemTable: ptr EfiSystemTable): EfiStatu
 
   # カーネルを読み込む
   var kernelFile: ptr EfiFileProtocol
-  discard rootDir[].open(rootDir, addr kernelFile, fastwidestr"\kernel.elf", efiFileModeRead, 0'u64)
+  status = rootDir[].open(rootDir, addr kernelFile, fastwidestr"\kernel.elf", efiFileModeRead, 0'u64)
+  if efiError(status):
+    print fastwidestr("failed to open file '\\kernel.elf': %r\n"), status
+    halt()
 
   var
     fileInfoSize = culonglong(sizeof(EfiFileInfo) + sizeof(uint16) * 12)
     fileInfoBuffer: array[sizeof(EfiFileInfo) + sizeof(uint16) * 12, uint8]
 
-  discard kernelFile[].getInfo(kernelFile, addr gEfiFileInfoGuid, addr fileInfoSize, addr fileInfoBuffer[0])
+  status = kernelFile[].getInfo(kernelFile, addr gEfiFileInfoGuid, addr fileInfoSize, addr fileInfoBuffer[0])
+  if efiError(status):
+    print fastwidestr("failed to get file information: %r\n"), status
 
   let fileInfo = cast[ptr EfiFileInfo](addr fileInfoBuffer[0])
   var
@@ -115,8 +143,13 @@ proc uefiMain(imageHandle: EfiHandle; systemTable: ptr EfiSystemTable): EfiStatu
 
     kernelBaseAddr: EfiPhysicalAddress = 0x10_0000'u64
 
-  discard gBS[].allocatePages(AllocateAddress, EfiLoaderData, (kernelFileSize + 0xfff) div 0x1000, addr kernelBaseAddr)
-  discard kernelFile[].read(kernelFile, addr kernelFileSize, cast[pointer](kernelBaseAddr))
+  status = gBS[].allocatePages(AllocateAddress, EfiLoaderData, (kernelFileSize + 0xfff) div 0x1000, addr kernelBaseAddr)
+  if efiError(status):
+    print fastwidestr("failed to allocate pages: %r\n"), status
+
+  status = kernelFile[].read(kernelFile, addr kernelFileSize, cast[pointer](kernelBaseAddr))
+  if efiError(status):
+    print fastwidestr("failed to read kernel file: %r\n"), status
 
   print fastwidestr("Kernel: 0x%0lx (%lu bytes)\n"), kernelBaseAddr, kernelFileSize
 
@@ -124,7 +157,7 @@ proc uefiMain(imageHandle: EfiHandle; systemTable: ptr EfiSystemTable): EfiStatu
   print fastwidestr("Kernel entry: 0x%0lx\n"), entryAddr
 
   # ブートローダーを閉じる
-  var status = gBS[].exitBootServices(imageHandle, memmap.mapKey)
+  status = gBS[].exitBootServices(imageHandle, memmap.mapKey)
   if efiError(status):
     status = getMemoryMap(memmap)
 
